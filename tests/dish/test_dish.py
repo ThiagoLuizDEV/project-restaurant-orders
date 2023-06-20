3b14dc7aa4d2f2395f1b3c5d51ef1713f0b8e54e
Thiago <thiago-470@outlook.com>
2023-06-19 23:04:29 -0300

    req02

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,37 @@
+from models.ingredient import Ingredient
+import pytest
 from src.models.dish import Dish  # noqa: F401, E261, E501
 
 
 # Req 2
 def test_dish():
-    pass
+    dish1 = Dish("lasanha berinjela", 27.00)
+    dish2 = Dish("açucar", 17.00)
+    other = Dish("", 10.00)
+
+    assert dish1.__hash__() == dish1.__hash__()
+    assert dish1.__hash__() != dish2.__hash__()
+    assert dish1.__repr__() == "Dish('lasanha berinjela', R$27.00)"
+    assert other.name == ''
+    assert dish1 == dish1
+
+    dish1.add_ingredient_dependency(Ingredient('queijo mussarela'), 200)
+    dish1.add_ingredient_dependency(Ingredient('farinha de trigo'), 200)
+    dish1.add_ingredient_dependency(Ingredient('água'), 200)
+    dish1.add_ingredient_dependency(Ingredient('berinjela'), 200)
+    dish1.add_ingredient_dependency(Ingredient('sal'), 8)
+    dish1.add_ingredient_dependency(Ingredient('tomate'), 200)
+
+    assert dish1.get_ingredients() == set([Ingredient('queijo mussarela'),
+                                           Ingredient('farinha de trigo'),
+                                           Ingredient('água'),
+                                           Ingredient('berinjela'),
+                                           Ingredient('sal'),
+                                           Ingredient('tomate'),
+                                           ])
+    assert dish1.get_restrictions() != set('')
+
+    with pytest.raises(TypeError):
+        Dish("lasanha berinjela", 'R$25.90')   
+    with pytest.raises(ValueError):
+        Dish("lasanha berinjela", -25.90)
